80bfa58bb8c83075d004c4c490410a8610f972cc
Armin Reichert <armin.reichert@web.de>
2022-05-18 13:25:33 +0200

    refac

--- src/main/java/de/amr/yt/pacman/model/GameModel.java
+++ src/main/java/de/amr/yt/pacman/model/GameModel.java
@@ -76,37 +76,37 @@ public class GameModel {
 		bonus = null;
 	}
 
-	public void setLevel(int n) {
-		if (n < 1) {
+	public void setLevel(int levelNumber) {
+		if (levelNumber < 1) {
 			throw new IllegalArgumentException("Level number must be at least 1");
 		}
-		level = switch (n) {
+		level = switch (levelNumber) {
 		//@formatter:off
-		case  1 -> new GameLevel(n, Bonus.CHERRIES,    100, 0.80f, 0.75f, 0.40f,  20, 0.80f, 10, 0.85f, 0.90f, 0.50f, 6, 5);
-		case  2 -> new GameLevel(n, Bonus.STRAWBERRY,  300, 0.90f, 0.85f, 0.45f,  30, 0.90f, 15, 0.95f, 0.95f, 0.55f, 5, 5);
-		case  3 -> new GameLevel(n, Bonus.PEACH,       500, 0.90f, 0.85f, 0.45f,  40, 0.90f, 20, 0.95f, 0.95f, 0.55f, 4, 5);
-		case  4 -> new GameLevel(n, Bonus.PEACH,       500, 0.90f, 0.85f, 0.45f,  40, 0.90f, 20, 0.95f, 0.95f, 0.55f, 3, 5);
-		case  5 -> new GameLevel(n, Bonus.APPLE,       700, 1.00f, 0.95f, 0.50f,  40, 1.00f, 20, 1.05f, 1.00f, 0.60f, 2, 5);
-		case  6 -> new GameLevel(n, Bonus.APPLE,       700, 1.00f, 0.95f, 0.50f,  50, 1.00f, 25, 1.05f, 1.00f, 0.60f, 5, 5);
+		case  1 -> new GameLevel(levelNumber, Bonus.CHERRIES,    100, 0.80f, 0.75f, 0.40f,  20, 0.80f, 10, 0.85f, 0.90f, 0.50f, 6, 5);
+		case  2 -> new GameLevel(levelNumber, Bonus.STRAWBERRY,  300, 0.90f, 0.85f, 0.45f,  30, 0.90f, 15, 0.95f, 0.95f, 0.55f, 5, 5);
+		case  3 -> new GameLevel(levelNumber, Bonus.PEACH,       500, 0.90f, 0.85f, 0.45f,  40, 0.90f, 20, 0.95f, 0.95f, 0.55f, 4, 5);
+		case  4 -> new GameLevel(levelNumber, Bonus.PEACH,       500, 0.90f, 0.85f, 0.45f,  40, 0.90f, 20, 0.95f, 0.95f, 0.55f, 3, 5);
+		case  5 -> new GameLevel(levelNumber, Bonus.APPLE,       700, 1.00f, 0.95f, 0.50f,  40, 1.00f, 20, 1.05f, 1.00f, 0.60f, 2, 5);
+		case  6 -> new GameLevel(levelNumber, Bonus.APPLE,       700, 1.00f, 0.95f, 0.50f,  50, 1.00f, 25, 1.05f, 1.00f, 0.60f, 5, 5);
 		case  7, 
-		      8 -> new GameLevel(n, Bonus.GRAPES,     1000, 1.00f, 0.95f, 0.50f,  50, 1.00f, 25, 1.05f, 1.00f, 0.60f, 2, 5);
-		case  9 -> new GameLevel(n, Bonus.GALAXIAN,   2000, 1.00f, 0.95f, 0.50f,  60, 1.00f, 30, 1.05f, 1.00f, 0.60f, 1, 3);
-		case 10 -> new GameLevel(n, Bonus.GALAXIAN,   2000, 1.00f, 0.95f, 0.50f,  60, 1.00f, 30, 1.05f, 1.00f, 0.60f, 5, 5);
-		case 11 -> new GameLevel(n, Bonus.BELL,       3000, 1.00f, 0.95f, 0.50f,  60, 1.00f, 30, 1.05f, 1.00f, 0.60f, 2, 5);
-		case 12 -> new GameLevel(n, Bonus.BELL,       3000, 1.00f, 0.95f, 0.50f,  80, 1.00f, 40, 1.05f, 1.00f, 0.60f, 1, 3);
-		case 13 -> new GameLevel(n, Bonus.KEY,        5000, 1.00f, 0.95f, 0.50f,  80, 1.00f, 40, 1.05f, 1.00f, 0.60f, 1, 3);
-		case 14 -> new GameLevel(n, Bonus.KEY,        5000, 1.00f, 0.95f, 0.50f,  80, 1.00f, 40, 1.05f, 1.00f, 0.60f, 3, 5);
+		      8 -> new GameLevel(levelNumber, Bonus.GRAPES,     1000, 1.00f, 0.95f, 0.50f,  50, 1.00f, 25, 1.05f, 1.00f, 0.60f, 2, 5);
+		case  9 -> new GameLevel(levelNumber, Bonus.GALAXIAN,   2000, 1.00f, 0.95f, 0.50f,  60, 1.00f, 30, 1.05f, 1.00f, 0.60f, 1, 3);
+		case 10 -> new GameLevel(levelNumber, Bonus.GALAXIAN,   2000, 1.00f, 0.95f, 0.50f,  60, 1.00f, 30, 1.05f, 1.00f, 0.60f, 5, 5);
+		case 11 -> new GameLevel(levelNumber, Bonus.BELL,       3000, 1.00f, 0.95f, 0.50f,  60, 1.00f, 30, 1.05f, 1.00f, 0.60f, 2, 5);
+		case 12 -> new GameLevel(levelNumber, Bonus.BELL,       3000, 1.00f, 0.95f, 0.50f,  80, 1.00f, 40, 1.05f, 1.00f, 0.60f, 1, 3);
+		case 13 -> new GameLevel(levelNumber, Bonus.KEY,        5000, 1.00f, 0.95f, 0.50f,  80, 1.00f, 40, 1.05f, 1.00f, 0.60f, 1, 3);
+		case 14 -> new GameLevel(levelNumber, Bonus.KEY,        5000, 1.00f, 0.95f, 0.50f,  80, 1.00f, 40, 1.05f, 1.00f, 0.60f, 3, 5);
 		case 15, 
-		     16 -> new GameLevel(n, Bonus.KEY,        5000, 1.00f, 0.95f, 0.50f, 100, 1.00f, 50, 1.05f, 1.00f, 0.60f, 1, 3);
-		case 17 -> new GameLevel(n, Bonus.KEY,        5000, 1.00f, 0.95f, 0.50f, 100, 1.00f, 50, 1.05f, 0.00f, 0.00f, 0, 0);
-		case 18 -> new GameLevel(n, Bonus.KEY,        5000, 1.00f, 0.95f, 0.50f, 100, 1.00f, 50, 1.05f, 1.00f, 0.60f, 1, 3);
+		     16 -> new GameLevel(levelNumber, Bonus.KEY,        5000, 1.00f, 0.95f, 0.50f, 100, 1.00f, 50, 1.05f, 1.00f, 0.60f, 1, 3);
+		case 17 -> new GameLevel(levelNumber, Bonus.KEY,        5000, 1.00f, 0.95f, 0.50f, 100, 1.00f, 50, 1.05f, 0.00f, 0.00f, 0, 0);
+		case 18 -> new GameLevel(levelNumber, Bonus.KEY,        5000, 1.00f, 0.95f, 0.50f, 100, 1.00f, 50, 1.05f, 1.00f, 0.60f, 1, 3);
 		case 19, 
-		     20 -> new GameLevel(n, Bonus.KEY,        5000, 1.00f, 0.95f, 0.50f, 120, 1.00f, 60, 1.05f, 0.00f, 0.00f, 0, 0);
-		default -> new GameLevel(n, Bonus.KEY,        5000, 0.90f, 0.95f, 0.50f, 120, 1.00f, 60, 1.05f, 0.00f, 0.00f, 0, 0);
+		     20 -> new GameLevel(levelNumber, Bonus.KEY,        5000, 1.00f, 0.95f, 0.50f, 120, 1.00f, 60, 1.05f, 0.00f, 0.00f, 0, 0);
+		default -> new GameLevel(levelNumber, Bonus.KEY,        5000, 0.90f, 0.95f, 0.50f, 120, 1.00f, 60, 1.05f, 0.00f, 0.00f, 0, 0);
 		//@formatter:on
 		};
 
-		if (n == 1) {
+		if (levelNumber == 1) {
 			score = 0;
 			lives = 3;
 			levelCounter.clear();
